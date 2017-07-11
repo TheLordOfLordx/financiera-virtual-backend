eeec3778d5182334001e3c58b7a5ca09ac8c0e8a
Armitage <listerine1989@gmail.com>
2017-07-11 12:59:21 -0500

    latest

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	dburl : "mongodb://daiAdmin:0126*dai*@localhost:27017/daimont",
 	secret : "$daimont.2017",
 	appPort : 8080,
-	base_url : 'http://www.daimont.com/'
+	base_url : 'http://www.daimont.com/',
 	smtp_email : "listerine1989@gmail.com",
 	smtp_password : 'house1989*',
 	smtp_server : 'smtp.gmail.com' 
