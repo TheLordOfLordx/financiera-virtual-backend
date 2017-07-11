651ffac8d170c4effdc151bf14a834d13127875f
Armitage <listerine1989@gmail.com>
2017-07-11 11:25:26 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -7,7 +7,7 @@ module.exports = function(app, apiRoutes){
     var _compiler = require(path.join(process.env.PWD , "helpers", "mailer.js"));
 
     var api_key = 'key-7060b4df5bc7256cbdbe3f0f4933414a';
-    var domain = 'www.daimont.com';
+    var domain = 'daimont.com';
     var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 
