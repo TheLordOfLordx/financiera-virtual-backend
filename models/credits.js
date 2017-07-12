a7533c58efa514db51ecb817edd3b3f5fba50299
Armitage <listerine1989@gmail.com>
2017-07-12 13:20:26 -0500

    latest

---
+++ models/credits.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var base_path = process.env.PWD;
+
+var entity = "credits";
+
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var metadata = require('./plugins/metadata');
+
+var _Schema = new Schema({
+	  data : Object
+});
+
+_Schema.pre('save', function (next) {
+  next();
+});
+
+//add plugins
+_Schema.plugin(metadata);
+_Schema.plugin(timestamps);
+
+module.exports = mongoose.model(entity, _Schema); 
