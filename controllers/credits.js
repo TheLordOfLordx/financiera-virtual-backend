af854dae6b79263923645a666310200086d754d3
Armitage <listerine1989@gmail.com>
2017-07-18 11:52:35 -0500

    latest

--- controllers/credits.js
+++ controllers/credits.js
@@ -1,89 +1,117 @@
 module.exports = function(app, apiRoutes, io){
-  	var _entity ="credits";
-  	var _url_alias = "credits";
+		var _entity ="credits";
+		var _url_alias = "credits";
 
-  	var path = require("path");
-    var mongoose = require('mongoose');
-    var Model = require(path.join("../", "models", _entity + ".js"));
+		var path = require("path");
+		var mongoose = require('mongoose');
+		var Model = require(path.join("../", "models", _entity + ".js"));
 
-    function get(req, res){
-      var REQ = req.params; 
+		function get(req, res){
+			var REQ = req.params; 
+			var where;
 
-       Model.find({ _user :  mongoose.Types.ObjectId(REQ.user) }).exec(function(err, rs){
-           if(!err){
-              res.status(200).json(rs);
-           }else{
-              res.json(err);
-           }
-       });
-    }
+			if(req.headers['x-daimont-user']){
+				where = { "metadata.author" : req.headers['x-daimont-user'] };
+			}
 
-
-
-    function getById(req, res){
-
-      var REQ = req.params; 
-
-       Model.findOne({ _id : REQ.id , _user :  mongoose.Types.ObjectId(REQ.user) }).exec(function(err, rs){
-           if(!err){
-              res.status(200).json(rs);
-           }else{
-              res.status(500).json(err);
-           }
-       });
-    }
-
-    function post(req, res){
-  		var data = {};
-  		var REQ = req.body || req.params;
-
-      !REQ.data || (data.data = REQ.data);
-
-      var model = new Model(data);
-
-  		model.save(function(err, rs){
-    			if(rs){
-    				res.status(200).json(rs);
-    			}else{
-    				res.status(500).json(err);
-    			}
-  		});
-    }
+			 Model.find( where || {} ).exec(function(err, rs){
+					if(!err){
+						res.status(200).json(rs);
+					}else{
+						res.json(err);
+					}
+			 });
+		}
 
 
-    function update(req, res){
-  		var data = {};
-  		var REQ = req.body || req.params;
 
-  		!REQ.data || (data.data = REQ.data); 
+		function getById(req, res){
+			var REQ = req.params; 
+			var where;
 
-  		data = { $set : data };          
+			if(req.headers['x-daimont-user']){
+				where = { "metadata.author" : req.headers['x-daimont-user'] };
+			}
 
-  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id), _user :  mongoose.Types.ObjectId(REQ.user)  }, data, function(err, rs){
-  			if(rs){
-  				res.status(200).json(rs);
-  			}else{
-          res.status(500).json(err)
-        }
-  		});
-    }
+			where._id = mongoose.Types.ObjectId(REQ.user);
+
+			Model.findOne(where || {}).exec(function(err, rs){
+				if(!err){
+					res.status(200).json(rs);
+				}else{
+					res.status(500).json(err);
+				}
+			});
+		}
+
+		function post(req, res){
+			var data = {};
+			var REQ = req.body || req.params;
+  			!REQ.metadata || (data.metadata = REQ.metadata);
+			!REQ.data || (data.data = REQ.data);
+
+			var model = new Model(data);
+
+			model.save(function(err, rs){
+				if(rs){
+					res.status(200).json(rs);
+				}else{
+					res.status(500).json(err);
+				}
+			});
+		}
 
 
-    function remove(req, res){
-        Model.remove({ _id : mongoose.Types.ObjectId(req.params.id) , _user :  mongoose.Types.ObjectId(REQ.user) }, function(err, rs){
-              if(!err){
-                  res.status(200).json(rs);
-              }else{
-                 res.status(500).json(err);
-              }
-        });
-	   }
+		function update(req, res){
+			var data = {};
+			var REQ = req.body || req.params;
+			var where;
 
-    apiRoutes.get("/" + _url_alias + "/:user", get);
-    apiRoutes.get("/" + _url_alias + "/:user/:id", getById);
-    apiRoutes.post("/" + _url_alias, post);
-    apiRoutes.put("/" + _url_alias + "/:user/:id", update);
-    apiRoutes.delete("/" + _url_alias + "/:user/:id", remove);
+			if(req.headers['x-daimont-user']){
+				where = { "metadata.author" : req.headers['x-daimont-user'] };
+			}
 
-    return this;
+			where._id = mongoose.Types.ObjectId(req.params.id);
+
+
+
+			!REQ.data || (data.data = REQ.data); 
+
+			data = { $set : data };          
+
+			Model.update( where , data , function(err, rs){
+				if(rs){
+					res.status(200).json(rs);
+				}else{
+					res.status(500).json(err)
+				}
+			});
+		}
+
+
+		function remove(req, res){
+			var where;
+
+			if(req.headers['x-daimont-user']){
+				where = { "metadata.author" : req.headers['x-daimont-user'] };
+			}
+
+			where._id = mongoose.Types.ObjectId(req.params.id);
+
+			Model.remove( where , function(err, rs){
+				if(!err){
+						res.status(200).json(rs);
+				}else{
+					 res.status(500).json(err);
+				}
+			});
+		 }
+
+		apiRoutes.get("/" + _url_alias , get);
+		apiRoutes.get("/" + _url_alias + "/:id", getById);
+		apiRoutes.post("/" + _url_alias, post);
+		apiRoutes.put("/" + _url_alias + "/:id", update);
+		apiRoutes.delete("/" + _url_alias + "/:id", remove);
+
+		return this;
 }
