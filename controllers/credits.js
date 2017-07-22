3b35dc02444af300a95a86694a57e4079af81fc9
Armitage <listerine1989@gmail.com>
2017-07-22 14:31:21 -0500

    latest

--- controllers/credits.js
+++ controllers/credits.js
@@ -11,7 +11,7 @@ module.exports = function(app, apiRoutes, io){
 			var where;
 
 			if(req.headers['x-daimont-user']){
-				where = { "metadata.author" : req.headers['x-daimont-user'] };
+				where = { "metadata.author" : req.headers['x-daimont-user'], "data.hidden" : false };
 			}
 
 			 Model.find( where || {} ).exec(function(err, rs){
