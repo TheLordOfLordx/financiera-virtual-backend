02afe4ee7e8710d917363e0122254db9b6e709dd
Armitage <listerine1989@gmail.com>
2017-07-30 12:06:57 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -6,6 +6,7 @@ module.exports = function(app, apiRoutes){
     var config = require(process.env.PWD + '/config.js');
     var moment = require('moment');
     moment.locale('es');
+    var formatCurrency = require('format-currency')
     var opts = { format: '%v %c', code: 'COP' }
 
     var User = require('../models/user');
